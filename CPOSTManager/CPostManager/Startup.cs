e26a3e170740b7923641fd028831394d250cdfb1
florianM54 <florianmmelo@gmail.com>
2016-11-29 17:03:21 +0800

    Add files via upload

---
+++ CPOSTManager/CPostManager/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup), "Configuration")]
+
+public class Startup
+{
+    public void Configuration(IAppBuilder app)
+    {
+        CPostManager.CPostManager.Startup.ConfigureSignalR(app);
+    }
+}
+
